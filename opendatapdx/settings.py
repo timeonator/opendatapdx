a5664d3956a1126249ca3316183dc604219de52c
timeonator <timeonator@gmail.com>
2021-06-06 14:39:08 -0700

    going to push a heroku

--- opendatapdx/settings.py
+++ opendatapdx/settings.py
@@ -89,27 +89,11 @@ DATABASES = {
         'HOST': os.getenv('DATABASE_HOST'),
         'PORT': '5432',
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'opendatapdx',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'postgres',
-    #     'HOST': 'db',
-    #     'PORT': 5432,
-    # }
-    # heroku settings
-
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'dd7enp07i6ibqk',
-    #     'USER': 'jvkocfgiupsgss',
-    #     'PASSWORD': '24f7fdd8a0818697f9ee180d297093ccf1795ad91632332fd5d578c78d67ff91',
-    #     'HOST': 'ec2-34-193-112-164.compute-1.amazonaws.com',
-    #     'PORT': 5432,
-    # }
 }
-# import dj_database_url
-# DATABASES['default']=dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+import dj_database_url
+DATABASES['default']=dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
