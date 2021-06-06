17d2716ad406f2c64a9ede08056b620a31edf6a1
timeonator <timeonator@gmail.com>
2021-06-06 15:29:51 -0700

    tweeking for heroku

--- opendatapdx/local_settings.py
+++ opendatapdx/local_settings.py
@@ -96,8 +96,8 @@ DATABASES = {
     #     'PORT': 5432,
     # }
 }
-# import dj_database_url
-# DATABASES['default']=dj_database_url.config(conn_max_age=600, ssl_require=True)
+import dj_database_url
+DATABASES['default']=dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
